71268ba69a9892e43ab16501a8ba94563ddd997e
ZiggyZiggyD <50599007+ZiggyZiggyD@users.noreply.github.com>
2020-04-30 17:35:38 -0400

    Update main_page.js

--- main_page.js
+++ main_page.js
@@ -350,9 +350,8 @@ function showLocalPlaylists() {
   else {
     for (i = 1; i < user_playlists.length; i++) {
       str += "<div class='addedPlaylist'>";
-      str += "<button class='playlist_button' value='" + user_playlists[i].name + "' onclick='getPlaylistSongs(this.value)'>" + user_playlists[i].name + "</button>";
-      str += "<button style='border: none'";
-      str += "class='like' value='" + user_playlists[i].name + "'onclick='deletePlaylist(this.value)'><i class='fa fa-minus' style='color:black; size: 10px'></i></button>";
+      str += "<button class='table-button' value='" + user_playlists[i].name + "' onclick='getPlaylistSongs(this.value)'>" + user_playlists[i].name + "</button>";
+      str += "<button type='button' value='" + user_playlists[i].name + "'onclick='deletePlaylist(this.value)'><i class='fa fa-minus' style='color:black; size: 10px'></i></button>";
       str += "</div><br>";
     };
       document.getElementById("list-homepage-songs").innerHTML = str;
